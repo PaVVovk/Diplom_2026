ac11e777f368f41e6081efbdc6624d88858b1f20
nikita9123467f65-cell <nikita9123467f65@gmail.com>
2026-04-02 21:56:29 +0300

    Заменил русский на английский

--- concentration.f90
+++ concentration.f90
@@ -10,15 +10,15 @@ integer, parameter :: dp = kind(1.0d0)
     logical :: repeat_flag
     real(dp) :: e, p, N, k_b, T_gas, t_max, t
     ! Ввод параметров
-    print *, 'Введите количество интервалов M:'
+    print *, 'Enter the number of intervals M:'
     read *, M
-    print *, 'Введите радиус трубки r0:'
+    print *, 'Enter the radius of the tube r0:'
     read *, r0
-    print *, 'Введите параметр сгущения delta (отрицательный для сгущения к центру):'
+    print *, 'Enter the DELTA thickening parameter:'
     read *, delta
-    print *, 'Введите числовой параметр'
+    print *, 'Enter a numeric parameter sigma'
     read *, sigma 
-    print *, 'Введите давление в паскалях'
+    print *, 'Enter the pressure in pascals'
     read *, p
     
     ! Выделение памяти
@@ -46,7 +46,7 @@ integer, parameter :: dp = kind(1.0d0)
     print *, 'N = ', N
     nu_ion = 0.8093E-16_dp*N
     !beta_ei = 10E-19_dp*(T_gas/300)**(-4.5_dp)*N
-    beta_ei = 10E-13
+    beta_ei = 10E-12
     k_e = 0.7890E+24_dp/N
     k_i = 0.286_dp + 0.669_dp*EXP(-E/(N*179.5_dp)) + 0.679_dp*EXP(-E/(N*1305_dp))
     D_e = 0.6932E+25_dp/N
@@ -79,42 +79,42 @@ integer, parameter :: dp = kind(1.0d0)
     end do
       ! Вывод результатов
     print *, '========================================'
-    print *, 'Параметры сетки:'
+    print *, 'Grid parameters:'
     print *, 'M =', M
     print *, 'r0 =', r0
     print *, 'delta =', delta
-    print *, 'Начальный шаг h_first =', h_first
+    print *, 'Initial step h_first =', h_first
     print *, '========================================'
-    print *, 'Основная сетка (узлы r_k):'
+    print *, 'Main mesh (r_k):'
     do k = 0, M
         print '(I4, F12.6)', k, r_k(k)
     end do
     
     print *, '========================================'
-    print *, 'Шаги основной сетки h_k:'
+    print *, 'Main grid steps h_k:'
     do k = 0, M-1
         print '(I4, F12.6)', k, h_k(k)
     end do
     
     print *, '========================================'
-    print *, 'Вспомогательная сетка (узлы r_{k+1/2}):'
+    print *, 'Auxiliary grid (r_{k+1/2}):'
     do k = 0, M-1
         print '(I4, F12.6)', k, r_half_k(k)
     end do
     
     print *, '========================================'
-    print *, 'Шаги вспомогательной сетки ℏ_k:'
+    print *, 'Auxiliary grid steps h_half_k:'
     do k = 1, M-1
         print '(I4, F12.6)', k, h_half_k(k)
     end do
     
     ! Проверка последнего узла
     print *, '========================================'
-    print *, 'Проверка: r(M) =', r_k(M), 'должно быть =', r0
+    print *, 'Examination: r(M) =', r_k(M), 'must be =', r0
     
-    print *, 'Минимальный шаг h_k:', minval(h_k)
+    print *, 'Min step h_k:', minval(h_k)
     if (minval(h_k) < 1e-30) then
-        print *, 'ВНИМАНИЕ: очень маленький шаг!'
+        print *, 'ATTENTION: very small step!'
     end if
     
     tau = minval(h_k**2/(4*D_e))  
@@ -137,9 +137,9 @@ integer, parameter :: dp = kind(1.0d0)
                                n_e_final, n_i_final, E_r_final, &
                                repeat_flag,  h_half_k, h_k)
         end do
-        print *, 'n_e_final = ', n_e_final(M/2)
-        print *, 'n_i_final = ', n_i_final(M/2)
-        print *, 'E_r_final = ', E_r_final(M/2)
+        !print *, 'n_e_final = ', n_e_final(M/2)
+        !print *, 'n_i_final = ', n_i_final(M/2)
+        !print *, 'E_r_final = ', E_r_final(M/2)
         n_e_i = n_e_final 
         n_i_i = n_i_final 
         E_r_i = E_r_final
@@ -150,13 +150,13 @@ integer, parameter :: dp = kind(1.0d0)
     
     print *, '========================================'
     do k = 0, M
-       print *, 'Концентрация электронов n_e =', n_e_i(k)
+       print *, 'Electron concentration n_e =', n_e_i(k)
     end do
     do k = 0, M
-       print *, 'Концентрация электронов n_i =', n_i_i(k)
+       print *, 'Ion concentration n_i =', n_i_i(k)
     end do
     do k = 0, M
-       print *, 'Концентрация электронов n_e =', E_r_i(k)
+       print *, 'Field E_r =', E_r_i(k)
     end do
     
     deallocate(r_k, r_half_k, h_k, h_half_k, n_e_i, n_i_i, &
@@ -379,13 +379,13 @@ contains
             error_E =  abs(E_r_m1(j) - E_r_m(j)) / max(abs(E_r_m1(j)), 1.0_dp)
             max_error = max(error_ne, error_ni, error_E, max_error)
         end do
-        print *, 'n_e_m1= ', n_e_m1(M/2)
-        print *, 'n_i_m1= ', n_i_m1(M/2)
-        print *, 'E_r_m1= ', E_r_m1(M/2)
-        print *, 'error_ne= ', error_ne
-        print *, 'error_ni= ', error_ni
-        print *, 'error_E= ',  error_E
-        print *, 'max_error= ', max_error
+       ! print *, 'n_e_m1= ', n_e_m1(M/2)
+        !print *, 'n_i_m1= ', n_i_m1(M/2)
+        !print *, 'E_r_m1= ', E_r_m1(M/2)
+       ! print *, 'error_ne= ', error_ne
+        !print *, 'error_ni= ', error_ni
+        !print *, 'error_E= ',  error_E
+        !print *, 'max_error= ', max_error
         do j = 0, M
             n_e_m(j) = n_e_m1(j)
             n_i_m(j) = n_i_m1(j)
