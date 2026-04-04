007b379206aeddff903835437ea376edc5c5c8ca
PaVVovk <vovk.pa@phystech.edu>
2026-04-04 17:51:34 +0300

    Added writing to file function

--- concentration.f90
+++ concentration.f90
@@ -1,14 +1,25 @@
 program concentration
-implicit none 
-integer, parameter :: dp = kind(1.0d0)
+implicit none
+    integer, parameter :: dp = kind(1.0d0)
     integer :: M, k, step_count
-    real(dp) :: r0, delta, h_first, sigma, tau 
+    integer, parameter :: T_count = 50
+    real(dp) :: r0, delta, h_first, sigma, tau
     real(dp), allocatable :: r_k(:), r_half_k(:), h_k(:), h_half_k(:)
-    real(dp), allocatable :: n_e_i(:), n_i_i(:), E_r_i(:), t_out(:)
+    real(dp) :: l_e, l_i, D_e, D_i, k_e, k_i, nu_ion
+    real(dp) :: p, N, t_max, t, t0, t_counter
+
+    real(dp), parameter :: k_b = 1.380649E-23_dp
+    real(dp), parameter :: e = 1.602176634E-19_dp
+    real(dp), parameter :: gamma_e = 0.7104_dp
+    real(dp), parameter :: gamma_i = 0.7104_dp
+    real(dp), parameter :: T_gas = 300_dp
+    real(dp), parameter :: beta_ei = 10E-12
+
+    real(dp), allocatable :: n_e_i(:), n_i_i(:), E_r_i(:)
     real(dp), allocatable :: n_e_final(:), n_i_final(:), E_r_final(:)
-    real(dp) :: gamma_e, l_e, gamma_i, l_i, D_e, D_i, k_e, k_i, nu_ion, beta_ei
+    real(dp) :: t_out(0:T_count)
     logical :: repeat_flag
-    real(dp) :: e, p, N, k_b, T_gas, t_max, t, t0
+
     ! Ввод параметров
     print *, 'Enter the number of intervals M:'
     read *, M
@@ -17,10 +28,10 @@ integer, parameter :: dp = kind(1.0d0)
     print *, 'Enter the DELTA thickening parameter:'
     read *, delta
     print *, 'Enter a numeric parameter sigma'
-    read *, sigma 
+    read *, sigma
     print *, 'Enter the pressure in pascals'
     read *, p
-    
+
     ! Выделение памяти
     allocate(r_k(0:M))
     allocate(r_half_k(0:M-1))
@@ -32,23 +43,14 @@ integer, parameter :: dp = kind(1.0d0)
     allocate(n_e_final(0:M))
     allocate(n_i_final(0:M))
     allocate(E_r_final(0:M))
-    allocate(t_out(0:M))
     !Элементарный заря
-    
-    e = 1.602176634E-19_dp
-    
+
     !Значения коэффициентов
-    gamma_e = 0.7104_dp
-    gamma_i = 0.7104_dp
-    T_gas = 300_dp
-    k_b = 1.380649E-23_dp
     N = p/(k_b*T_gas)
     print *, 'N = ', N
     nu_ion = 0.8093E-16_dp*N
-    !beta_ei = 10E-19_dp*(T_gas/300)**(-4.5_dp)*N
-    beta_ei = 10E-12
     k_e = 0.7890E+24_dp/N
-    k_i = 0.286_dp + 0.669_dp*EXP(-E/(N*179.5_dp)) + 0.679_dp*EXP(-E/(N*1305_dp))
+    k_i = 0.286_dp + 0.669_dp*EXP(-E/(N*179.5_dp)) + 0.679_dp*EXP(-E/(N*1305_dp)) !см^2 / (В * с)
     D_e = 0.6932E+25_dp/N
     D_i = k_i*T/e
     l_e = 1/(N*10E-20_dp)
@@ -59,21 +61,21 @@ integer, parameter :: dp = kind(1.0d0)
     t = 0
     t_max = 10E-4
     step_count = 0
-  
-    
+
+
     ! Генерация основной сетки
     r_k(0) = 0.0_dp
-    if(delta == 0) then 
-        h_first = r0/M       
-    else    
+    if(delta == 0) then
+        h_first = r0/M
+    else
         h_first = (delta * r0) / ((1.0_dp + delta)**M - 1.0_dp)
-    end if 
+    end if
     do k = 0, M-1
         h_k(k) = h_first * (1.0_dp + delta)**(k)
         r_k(k+1) = r_k(k) + h_k(k)
         r_half_k(k) = (r_k(k) + r_k(k+1)) / 2.0_dp
     end do
-        
+
     do k = 1, M-1
         h_half_k(k) = (h_k(k-1) + h_k(k)) / 2.0_dp
     end do
@@ -89,47 +91,49 @@ integer, parameter :: dp = kind(1.0d0)
     do k = 0, M
         print '(I4, F12.6)', k, r_k(k)
     end do
-    
+
     print *, '========================================'
     print *, 'Main grid steps h_k:'
     do k = 0, M-1
         print '(I4, F12.6)', k, h_k(k)
     end do
-    
+
     print *, '========================================'
     print *, 'Auxiliary grid (r_{k+1/2}):'
     do k = 0, M-1
         print '(I4, F12.6)', k, r_half_k(k)
     end do
-    
+
     print *, '========================================'
     print *, 'Auxiliary grid steps h_half_k:'
     do k = 1, M-1
         print '(I4, F12.6)', k, h_half_k(k)
     end do
-    
+
     ! Проверка последнего узла
     print *, '========================================'
     print *, 'Examination: r(M) =', r_k(M), 'must be =', r0
-    
+
     print *, 'Min step h_k:', minval(h_k)
     if (minval(h_k) < 1e-30) then
         print *, 'ATTENTION: very small step!'
-    end if 
-    
+    end if
+
     !Заполняем массив t_out
-    
-    t0 = 10E-6
-    do k = 0, M
-        t_out(k) = 10**(k/10)*t0
-    end do     
-    
+
+    t0 = t_max / 10 ** (T_count / 10)
+    do k = 0, T_count
+        t_out(k) = t0 * 10 ** (k / 10)
+        print *, t_out(k)
+    end do
+
     tau = minval(h_k**2/(4*D_e))
-     
+
+    t_counter = 0
     do while(t < t_max)
         repeat_flag = .false.
         step_count = step_count + 1
-    
+
         if (mod(step_count, 10) == 0) then
             print *, 'Step:', step_count, 't =', t, 'tau =', tau
         end if
@@ -145,22 +149,22 @@ integer, parameter :: dp = kind(1.0d0)
                                n_e_final, n_i_final, E_r_final, &
                                repeat_flag,  h_half_k, h_k)
         end do
-        m
-        if (t>t_out)) then
-            call ...
-            m = m+1
+
+        if (t > t_out(t_counter)) then
+            call show_parameters(n_e_final, n_i_final, E_r_final, t)
+            t_counter = t_counter+1
         end if
         !print *, 'n_e_final = ', n_e_final(M/2)
         !print *, 'n_i_final = ', n_i_final(M/2)
         !print *, 'E_r_final = ', E_r_final(M/2)
-        n_e_i = n_e_final 
-        n_i_i = n_i_final 
+        n_e_i = n_e_final
+        n_i_i = n_i_final
         E_r_i = E_r_final
         t = t + tau
-    end do                                          
-    
+    end do
 
-    
+
+
     print *, '========================================'
     do k = 0, M
        print *, 'Electron concentration n_e =', n_e_i(k)
@@ -171,15 +175,15 @@ integer, parameter :: dp = kind(1.0d0)
     do k = 0, M
        print *, 'Field E_r =', E_r_i(k)
     end do
-    
+
     deallocate(r_k, r_half_k, h_k, h_half_k, n_e_i, n_i_i, &
-               E_r_i, n_e_final, n_i_final, E_r_final, t_out)
-contains 
-    
+               E_r_i, n_e_final, n_i_final, E_r_final)
+contains
+
     !Прогонка для электронов
-    
+
     subroutine solve_electron_continuity(M, sigma, tau, h_k, h_half_k, r_k, &
-                                      r_half_k, gamma_e, l_e, n_e_m1, D_e, & 
+                                      r_half_k, gamma_e, l_e, n_e_m1, D_e, &
                                       nu_ion, beta_ei, k_e, n_i_im, &
                                       n_e_i, E_r_im)
     implicit none
@@ -190,23 +194,23 @@ contains
     real(dp), intent(in) :: n_i_im(0:M), E_r_im(0:M)
     real(dp), intent(in) :: gamma_e, l_e, D_e, nu_ion, beta_ei, k_e
     real(dp), intent(out) :: n_e_m1(0:M)
-    
+
     integer :: k
-    real(dp) :: A(0:M), B(0:M), C(0:M), F(0:M) 
-    real(dp) :: alpha(0:M+1), beta(0:M+1)       
-    real(dp) :: y(0:M)   
-  
+    real(dp) :: A(0:M), B(0:M), C(0:M), F(0:M)
+    real(dp) :: alpha(0:M+1), beta(0:M+1)
+    real(dp) :: y(0:M)
+
     real(dp) :: a_k, b_k, u_k, w_k
-    
-   
+
+
     B(0) = 4.0_dp*D_e/(h_k(0)**2) + 2.0_dp*k_e*E_r_im(1)/h_k(0)
     C(0) = 1.0_dp/(sigma*tau) - nu_ion + beta_ei * n_i_im(0) + 4.0_dp * D_e / (h_k(0)**2)
     F(0) = n_e_i(0)/tau + (1.0_dp - sigma) * (B(0)*n_e_i(1) - (C(0) - 1.0_dp/(sigma*tau))*n_e_i(0))
 
-    
+
     do k = 1, M-1
-        a_k = (r_half_k(k-1)/r_k(k)) * (1.0_dp/h_half_k(k))  
-        b_k = (r_half_k(k+1)/r_k(k)) * (1.0_dp/h_half_k(k))    
+        a_k = (r_half_k(k-1)/r_k(k)) * (1.0_dp/h_half_k(k))
+        b_k = (r_half_k(k+1)/r_k(k)) * (1.0_dp/h_half_k(k))
         u_k = (b_k - a_k) / 2.0_dp
         w_k = b_k / h_k(k) + a_k / h_k(k-1)
         A(k) = a_k * (D_e/h_k(k-1) - k_e*E_r_im(k-1)/2.0_dp)
@@ -214,14 +218,14 @@ contains
         C(k) = 1.0_dp/(sigma*tau) - nu_ion + beta_ei * n_i_im(k) + w_k * D_e - u_k * k_e * E_r_im(k)
         F(k) = n_e_i(k)/tau + (1.0_dp - sigma) * (A(k)*n_e_i(k-1) - (C(k) - 1.0_dp/(sigma*tau))*n_e_i(k) + B(k)*n_e_i(k+1))
     end do
-    	
+
     A(M) = gamma_e*l_e/h_k(M-1)
     C(M) = 1.0_dp + gamma_e * l_e / h_k(M-1)
     B(M) = 0.0_dp  ! B_M не используется
     F(M) = 0.0_dp
-    
+
     ! Метод прогонки
-     
+
     ! Прямой ход
     alpha(1) = B(0) / C(0)
     beta(1) = F(0) / C(0)
@@ -229,26 +233,26 @@ contains
     !Сохранить знаменатель в отдельную перем
         alpha(k+1) = B(k) / (C(k) - alpha(k) * A(k))
     end do
-    
+
     do k = 1, M
         beta(k+1) = (beta(k) * A(k) + F(k)) / (C(k) - alpha(k) * A(k))
     end do
-    ! Обратный ход 
+    ! Обратный ход
     y(M) = beta(M+1)
     do k = M-1, 0, -1
         y(k) = alpha(k+1) * y(k+1) + beta(k+1)
     end do
-    
+
     do k = 0, M
         n_e_m1(k) = y(k) / sigma
     end do
-    
-    end subroutine solve_electron_continuity 
-    
-    !Прогонка для ионов 
-		
+
+    end subroutine solve_electron_continuity
+
+    !Прогонка для ионов
+
     subroutine solve_ion_continuity(M, sigma, tau, h_k, h_half_k, r_k, &
-                                      r_half_k, gamma_i, n_i_i, l_i, n_i_m1, D_i, & 
+                                      r_half_k, gamma_i, n_i_i, l_i, n_i_m1, D_i, &
                                       nu_ion, n_e_im, E_r_im,beta_ei, k_i)
     integer, intent(in) :: M
     real(dp), intent(in) :: sigma, tau
@@ -256,22 +260,22 @@ contains
     real(dp), intent(in) :: n_e_im(0:M), E_r_im(0:M), n_i_i(0:M)
     real(dp), intent(in) :: gamma_i, l_i, D_i, nu_ion, beta_ei, k_i
     real(dp), intent(out) :: n_i_m1(0:M)
-    
+
     integer :: k
-    real(dp) :: A(0:M), B(0:M), C(0:M), F(0:M) 
-    real(dp) :: alpha(0:M+1), beta(0:M+1)       
-    real(dp) :: y(0:M)   
-  
+    real(dp) :: A(0:M), B(0:M), C(0:M), F(0:M)
+    real(dp) :: alpha(0:M+1), beta(0:M+1)
+    real(dp) :: y(0:M)
+
     real(dp) :: a_k, b_k, u_k, w_k
-    
-    
+
+
     B(0) = 4.0_dp*D_i/(h_k(0)**2) - 2.0_dp*k_i*E_r_im(1)/h_k(0)
     C(0) = 1.0_dp/(sigma*tau) + beta_ei*n_e_im(0) + 4.0_dp*D_i/(h_k(0)**2)
     F(0) = n_i_i(0)/tau + nu_ion*n_e_im(0) + &
            (1-sigma)*(B(0)*n_i_i(1) - (C(0) - 1/(sigma*tau))*n_i_i(0))
     do k = 1, M-1
-        a_k = (r_half_k(k-1)/r_k(k)) * (1.0_dp/h_half_k(k))  
-        b_k = (r_half_k(k+1)/r_k(k)) * (1.0_dp/h_half_k(k))    
+        a_k = (r_half_k(k-1)/r_k(k)) * (1.0_dp/h_half_k(k))
+        b_k = (r_half_k(k+1)/r_k(k)) * (1.0_dp/h_half_k(k))
         u_k = (b_k - a_k) / 2.0_dp
         w_k = b_k / h_k(k) + a_k / h_k(k-1)
         A(k) = a_k*(D_i/h_k(k-1) + k_i*E_r_im(k-1)/2)
@@ -284,30 +288,30 @@ contains
     A(M) = gamma_i * l_i / h_k(M-1)
     C(M) = 1.0_dp + gamma_i * l_i / h_k(M-1)
     B(M) = 0.0_dp
-    F(M) = 0.0_dp   
-    
+    F(M) = 0.0_dp
+
     ! Прямой ход
     alpha(1) = B(0) / C(0)
     beta(1) = F(0) / C(0)
     do k = 1, M-1
         alpha(k+1) = B(k) / (C(k) - alpha(k) * A(k))
     end do
-    
+
     do k =1, M
         beta(k+1) = (beta(k) * A(k) + F(k)) / (C(k) - alpha(k) * A(k))
     end do
-    ! Обратный ход 
+    ! Обратный ход
     y(M) = beta(M+1)
     do k = M-1, 0, -1
         y(k) = alpha(k+1) * y(k+1) + beta(k+1)
     end do
-    
+
     do k = 0, M
         n_i_m1(k) = y(k) / sigma
     end do
-       
-    end subroutine solve_ion_continuity 
-    
+
+    end subroutine solve_ion_continuity
+
     !Решение уравнения Пуассона
     subroutine solve_poisson_trapezoidal(M, r_k, h_k, n_e_m1, n_i_m1, E_r_m1)
     implicit none
@@ -315,31 +319,31 @@ contains
     real(dp), intent(in) :: r_k(0:M), h_k(0:M-1)
     real(dp), intent(in) :: n_e_m1(0:M), n_i_m1(0:M)
     real(dp), intent(out) :: E_r_m1(0:M)
-    
+
     integer :: k
-    real(dp) :: F(1:M), const   
+    real(dp) :: F(1:M), const
     const = 2.0_dp*3.141592653589793_dp*1.602176634E-19_dp
     E_r_m1(0) = 0
-    E_r_m1(1) = const*h_k(0)*((n_i_m1(1)-n_e_m1(1)) + & 
+    E_r_m1(1) = const*h_k(0)*((n_i_m1(1)-n_e_m1(1)) + &
                  (n_i_m1(0) - n_e_m1(0)))
     F(1) = r_k(1)*E_r_m1(1)
     do k = 2, M
         F(k) = F(k-1) + const*h_k(k-1)*(r_k(k-1)*(n_i_m1(k-1)-n_e_m1(k-1))+ &
                r_k(k)*(n_i_m1(k) - n_e_m1(k)))
-        E_r_m1(k) = F(k)/r_k(k)        
-    end do  
+        E_r_m1(k) = F(k)/r_k(k)
+    end do
     end subroutine solve_poisson_trapezoidal
-    
-    !Итерации 
-    
+
+    !Итерации
+
     subroutine solve_iterations(M, r_k, r_half_k, sigma, tau, gamma_e, l_e, gamma_i, l_i, &
                            D_e, D_i, k_e, k_i, nu_ion, beta_ei, &
                            n_e_i, n_i_i, E_r_i, &
                            n_e_final, n_i_final, E_r_final, &
-                           repeat_flag, h_half_k, h_k) 
-    
+                           repeat_flag, h_half_k, h_k)
+
     ! Входные параметры
-    
+
     integer, intent(in) :: M
     real(dp), intent(in) :: sigma, r_k(0:M), r_half_k(0:M-1), h_half_k(1:M-1), h_k(0:M-1)
     real(dp), intent(in) :: gamma_e, l_e, gamma_i, l_i
@@ -347,45 +351,45 @@ contains
     real(dp), intent(in) :: n_e_i(0:M), n_i_i(0:M), E_r_i(0:M)
     logical, intent(inout) :: repeat_flag
     real(dp), intent(inout) :: tau
-    
+
     ! Выходные параметры
-    
+
     real(dp), intent(out) :: n_e_final(0:M), n_i_final(0:M), E_r_final(0:M)
-    
-    !Локальные переменные 
-    
+
+    !Локальные переменные
+
     integer :: iter, k, j
     real(dp) :: error_ne, error_ni, error_E
     real(dp) :: n_e_m(0:M), n_i_m(0:M), E_r_m(0:M)
     real(dp) :: n_e_im(0:M), n_i_im(0:M), E_r_im(0:M)
     real(dp) :: n_e_m1(0:M), n_i_m1(0:M), E_r_m1(0:M)
     real(dp) :: max_error, tol
-     
+
     n_e_m = n_e_i
     n_i_m = n_i_i
     E_r_m = E_r_i
-    
+
     tol = 10E-8
     iter = 10
-    
-    
+
+
     do k = 1, iter
         max_error = 0.0_dp
         do j = 0, M
             n_e_im(j) = sigma * n_e_m(j) + (1.0_dp - sigma) * n_e_i(j)
             n_i_im(j) = sigma * n_i_m(j) + (1.0_dp - sigma) * n_i_i(j)
-            E_r_im(j) = sigma * E_r_m(j) + (1.0_dp - sigma) * E_r_i(j)  
+            E_r_im(j) = sigma * E_r_m(j) + (1.0_dp - sigma) * E_r_i(j)
         end do
         call solve_electron_continuity(M, sigma, tau, h_k, h_half_k, r_k, &
-                                      r_half_k, gamma_e, l_e, n_e_m1, D_e, & 
+                                      r_half_k, gamma_e, l_e, n_e_m1, D_e, &
                                       nu_ion, beta_ei, k_e, n_i_im, &
-                                      n_e_i, E_r_im)  
-  
+                                      n_e_i, E_r_im)
+
         call solve_ion_continuity(M, sigma, tau, h_k, h_half_k, r_k, &
-                                      r_half_k, gamma_i, n_i_i, l_i, n_i_m1, D_i, & 
-                                      nu_ion, n_e_im, E_r_im,beta_ei, k_i) 
+                                      r_half_k, gamma_i, n_i_i, l_i, n_i_m1, D_i, &
+                                      nu_ion, n_e_im, E_r_im,beta_ei, k_i)
         call solve_poisson_trapezoidal(M, r_k, h_k, n_e_m1, n_i_m1, E_r_m1)
-         
+
          do j = 0, M
             error_ne = abs(n_e_m1(j) - n_e_m(j)) / max(abs(n_e_m1(j)), 1.0_dp)
             error_ni = abs(n_i_m1(j) - n_i_m(j)) / max(abs(n_i_m1(j)), 1.0_dp)
@@ -405,34 +409,65 @@ contains
             E_r_m(j) = E_r_m1(j)
         end do
         if (max_error < tol) then
-            n_e_final = n_e_m1 
+            n_e_final = n_e_m1
             n_i_final = n_i_m1
             E_r_final = E_r_m1
-            exit  
-        end if     
+            exit
+        end if
         if (k == iter) then
-            tau = tau/2 
+            tau = tau/2
             print *, 'tau = ', tau
             repeat_flag = .true.
-        else 
-            repeat_flag = .false.    
-        end if  
+        else
+            repeat_flag = .false.
+        end if
     end do
-    end subroutine solve_iterations  
-    subroutine show_parametrs(M, r_k, r_half_k, sigma, tau, gamma_e, l_e, gamma_i, l_i, &
-                           D_e, D_i, k_e, k_i, nu_ion, beta_ei, &
-                           n_e_i, n_i_i, E_r_i, &
-                           n_e_final, n_i_final, E_r_final, &
-                           repeat_flag, h_half_k, h_k)    
-    real(dp), intent(in):
-         potential(0) = 0
-         do k = 1, M
-             potential(k) = -(E_r_final(k-1) + E_r_final(k))*h_k(k-1)/2
-         end do
-         
-         do k = 0, M-1
-             J_e(k) = 
-             J_i(k) = 
-         end do                         
-    end subroutine show_parametrs      
+    end subroutine solve_iterations
+
+    function df_dr(f1, f2, f3, x1, x2, x3, delta1, delta2)
+        real(dp) :: f1, f2, f3, x1, x2, x3, delta1, delta2, df_dr
+        df_dr = ((f2 - f1) / delta1 ** 2 + (f3 - f2) / delta2 ** 2) / (1 / delta1 + 1 / delta2)
+    end function df_dr
+
+    subroutine show_parameters(n_e, n_i, E_r, time)
+        real(dp), intent(in) :: time
+        real(dp), intent(in) :: n_e(0:M), n_i(0:M), E_r(0:M)
+        real(dp) :: potential(0:M), j_e(0:M), j_i(0:M)
+        character(len=10) :: time_name
+
+        write (time_name, '(F7.5)') time
+        open(11, file = 'potential_t='//trim(time_name)//'.txt', status = 'new')
+        open(12, file = 'j_e_t='//trim(time_name)//'.txt', status = 'new')
+        open(13, file = 'j_i_t='//trim(time_name)//'.txt', status = 'new')
+
+        potential(M) = 0
+        do k = M, 1, -1
+            potential(k-1) = potential(k) + (E_r_final(k) + E_r_final(k-1)) * h_k(k-1) / 2.0_dp
+        end do
+
+        j_e(0) = 0
+        j_i(0) = 0
+
+        do k = 1, M-1
+            j_e(k) = - D_e * df_dr(n_e(k-1), n_e(k), n_e(k+1), r_k(k-1), r_k(k), r_k(k+1), &
+            h_k(k-1), h_k(k)) - k_e * n_e(k) * E_r(k)
+
+            j_i(k) = - D_i * df_dr(n_i(k-1), n_i(k), n_i(k+1), r_k(k-1), r_k(k), r_k(k+1), &
+            h_k(k-1), h_k(k)) + k_i * n_i(k) * E_r(k)
+        end do
+
+        j_e(M) = -D_i * (n_e(M) - n_e(M-1)) / h_k(M-1) - k_e * n_e(M) * E_r(M)
+        j_i(M) = -D_i * (n_i(M) - n_i(M-1)) / h_k(M-1) + k_i * n_i(M) * E_r(M)
+
+        do k = 0, M
+            write (11, *) potential(k)
+            write (12, *) j_e(k)
+            write (13, *) j_i(k)
+        end do
+
+        close(11)
+        close(12)
+        close(13)
+
+    end subroutine show_parameters
 end program concentration
